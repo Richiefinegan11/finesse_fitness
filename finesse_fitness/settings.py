fc4f10a5cdb489bf6eab10903067d3df625826c7
Richiefinegan11 <richiefinegan1990@gmail.com>
2021-02-08 11:34:59 +0000

    Added content to home template and style, changed database settings

--- finesse_fitness/settings.py
+++ finesse_fitness/settings.py
@@ -104,11 +104,11 @@ WSGI_APPLICATION = 'finesse_fitness.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -151,3 +151,4 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
