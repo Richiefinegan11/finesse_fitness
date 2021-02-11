d346d89c86e58307c6bd4dd79ae7cae022903c0e
Richiefinegan11 <richiefinegan1990@gmail.com>
2021-02-11 15:05:25 +0000

    Added back to top functionality to product page

--- products/static/products/js/shop.js
+++ products/static/products/js/shop.js
@@ -1,8 +1,13 @@
-// Sort trigger button
-$("#sort-container").hide()
-$( "#sort-trigger").click(function() {     
-    if($('#sort-container:visible').length)
-        $('#sort-container').hide("slide", { direction: "right" }, 1000);
-    else
-        $('#sort-container').show("slide", { direction: "right" }, 1000);        
+// brings user back to top
+$(".btt-link").click(function () {
+  $("html, body").animate({ scrollTop: 0 }, 500);
+});
+
+// Hides the sorting icon links
+$("#sort-container").hide();
+// Makes the sorting icon links visable/hidden when trigger is clicked
+$("#sort-trigger").click(function () {
+  if ($("#sort-container:visible").length)
+    $("#sort-container").hide("slide", { direction: "right" }, 1000);
+  else $("#sort-container").show("slide", { direction: "right" }, 1000);
 });
