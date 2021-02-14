a8b2551f989bf003f95ba06b015c9d211678d632
Richiefinegan11 <richiefinegan1990@gmail.com>
2021-02-14 14:33:34 +0000

    Added builtins for crispy forms to settings.py

--- finesse_fitness/settings.py
+++ finesse_fitness/settings.py
@@ -84,7 +84,8 @@ TEMPLATES = [
                 'bag.contexts.bag_contents',
             ],
             'builtins': [
-                
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
             ]
         },
     },
