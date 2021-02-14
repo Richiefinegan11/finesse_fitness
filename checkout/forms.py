3b82b281a0e3bf3a29fc482306d71f2d238b934b
Richiefinegan11 <richiefinegan1990@gmail.com>
2021-02-14 14:44:04 +0000

    Added more style to and functionality to checkout page

--- checkout/forms.py
+++ checkout/forms.py
@@ -1,8 +1,9 @@
 from django import forms
 from .models import Order
 
+
 class OrderForm(forms.ModelForm):
-    class meta:
+    class Meta:
         model = Order
         fields = ('full_name', 'email', 'phone_number',
                   'street_address1', 'street_address2',
--- checkout/views.py
+++ checkout/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import redirect, render, reverse
+from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
 
-from .forms import OrderForm                                                                                                                                                                                                                                                                                                                                                                                             
+from .forms import OrderForm
+
 
 def checkout(request):
     bag = request.session.get('bag', {})
