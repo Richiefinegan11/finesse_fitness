7e645d1b2cd1de3922bb09e8ee94f422bc7aaace
Richiefinegan11 <richiefinegan1990@gmail.com>
2021-02-19 19:10:49 +0000

    Change UserProfile models, added logic to views, created forms for profile, added content to profile template and style

--- profiles/models.py
+++ profiles/models.py
@@ -11,18 +11,18 @@ class UserProfile(models.Model):
     information and order history
     """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    full_name = models.CharField(max_length=50, null=True, blank=True)
-    email = models.EmailField(max_length=254, null=True, blank=True)
-    phone_number = models.CharField(max_length=20, null=True, blank=True)
-    street_address1 = models.CharField(max_length=80, null=True, blank=True)
-    street_address2 = models.CharField(max_length=80, null=True, blank=True)
-    town_or_city = models.CharField(max_length=40, null=True, blank=True)
-    county = models.CharField(max_length=80, null=True, blank=True)
-    postcode = models.CharField(max_length=20, null=True, blank=True)
-    country = CountryField(blank_label='Country', null=True, blank=True)
+    user_full_name = models.CharField(max_length=50, null=True, blank=True)
+    user_email = models.EmailField(max_length=254, null=True, blank=True)
+    user_phone_number = models.CharField(max_length=20, null=True, blank=True)
+    user_street_address1 = models.CharField(max_length=80, null=True, blank=True)
+    user_street_address2 = models.CharField(max_length=80, null=True, blank=True)
+    user_town_or_city = models.CharField(max_length=40, null=True, blank=True)
+    user_county = models.CharField(max_length=80, null=True, blank=True)
+    user_postcode = models.CharField(max_length=20, null=True, blank=True)
+    user_country = CountryField(blank_label='Country', null=True, blank=True)
 
     def __str__(self):
-        return self.user.username
+        return self.user_username
 
 
 @receiver(post_save, sender=User)
