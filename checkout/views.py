b1dcee60342e3e147eba8ce8abaf407757b1c2c1
Richiefinegan11 <richiefinegan1990@gmail.com>
2021-02-20 07:49:54 +0000

    Updated webhook handler to handle profiles

--- checkout/views.py
+++ checkout/views.py
@@ -109,13 +109,13 @@ def checkout(request):
                 order_form = OrderForm(initial={
                     'full_name': profile.user.get_full_name(),
                     'email': profile.user.email,
-                    'phone_number': profile.default_phone_number,
-                    'country': profile.default_country,
-                    'postcode': profile.default_postcode,
-                    'town_or_city': profile.default_town_or_city,
-                    'street_address1': profile.default_street_address1,
-                    'street_address2': profile.default_street_address2,
-                    'county': profile.default_county,
+                    'phone_number': profile.user_phone_number,
+                    'country': profile.user_country,
+                    'postcode': profile.user_postcode,
+                    'town_or_city': profile.user_town_or_city,
+                    'street_address1': profile.user_street_address1,
+                    'street_address2': profile.user_street_address2,
+                    'county': profile.user_county,
                 })
             except UserProfile.DoesNotExist:
                 order_form = OrderForm()
