902361cfd5da237d7bd520292f9fe3911235207d
Richiefinegan11 <richiefinegan1990@gmail.com>
2021-02-21 21:48:09 +0000

    Added views and urls to subscription

--- subscriptions/urls.py
+++ subscriptions/urls.py
@@ -4,5 +4,12 @@ from . import views
 urlpatterns = [
     path('', views.subscriptions, name='subscriptions'),
     path('subscription_type/', views.subscription_type, name='subscription_type'),
-    path('subscription_checkout/', views.subscription_chekout, name='subscription_chekout'),
+    path('subscription_checkout/', views.subscription_checkout,
+         name='subscription_checkout'),
+    path('user_subscription/', views.user_subscription_view,
+         name="user_subscription"),
+    path('subscription_change/', views.subscription_change,
+         name="subscription_change"),
+    path('subscription_update/', views.subscription_update,
+         name="subscription_update"),
 ]
--- subscriptions/views.py
+++ subscriptions/views.py
@@ -1,11 +1,18 @@
-from django.db import models
-from django.shortcuts import redirect, render, reverse
+import stripe
+
 from django.conf import settings
-from django.utils.translation import templatize
+from django.db import models
+from django.shortcuts import redirect, render, reverse, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_exempt
+from django.http.response import JsonResponse, HttpResponse
+
 
 from .models import Subscription, StripeCustomer
 from profiles.models import UserProfile
+from django.contrib import messages
 from django.contrib.auth.models import User
+
 
 def subscriptions(request):
     """
@@ -37,7 +44,7 @@ def subscriptions(request):
 
 def subscription_type(request):
     """
-    Catch the membership type selected by the user, 
+    Catch the subscription type selected by the user, 
     store it in the session and redirect the user to signup
     """
 
@@ -49,5 +56,149 @@ def subscription_type(request):
     return redirect(reverse('account_signup'))
 
 
+@login_required
+def subscription_checkout(request):
+    """
+    Get user's selected subscription, display it and allow the user
+    to change the subscription
+    """
+
+    # Get all subscriptions
+    all_subscriptions = Subscription.objects.all()
+
+    # Check if the user has a subscription, if not re-direct to
+    # subscription change
+    profile = UserProfile.objects.get(user=request.user)
+    if profile.subscription:
+        return redirect(reverse('subscription_change'))
+    # If user is updating selected subscription, the
+    # subscription_type to the new value
+    if request.GET.get('subscription-new'):
+        subscription_type = request.GET.get('subscription-new')
+        # add subscription type to session to retrieve for stripe
+        request.session['subscription'] = subscription_type
+    # If user logged in after registering, get subscription_type
+    # from session
+    else:
+        try:
+            # get user selected subscription
+            subscription_type = request.session['subscription']
+        except KeyError:
+            # If user logged in normally, redirect them
+            # to the profile page
+            return redirect(reverse('profile'))
+
+    # Retrieve data for selected subscription type
+    subscription = get_object_or_404(Subscription, name=subscription_type)
+
+    template = 'subscriptions/subscription_checkout.html'
+    context = {
+        'subscription': subscription,
+        'all_subscriptions': all_subscriptions,
+    }
+
+    return render(request, template, context)
 
 
+@login_required
+def user_subscription_view(request):
+    """
+    Display user's subscription view with details
+    """
+    profile = UserProfile.objects.get(user=request.user)
+
+    if not profile.subscription:
+        messages.error(
+            request, "You haven't subscrtibed yet. Please choose one and join the Finesse Fitness Family")
+        return redirect(reverse('subscriptions'))
+
+    subscription = get_object_or_404(Subscription, name=profile.subscriptions)
+    context = {
+        'subscription': subscription
+    }
+    template = 'subscription/user_subscription.html'
+
+    return render(request, template, context)
+
+
+@login_required
+def subscription_change(request):
+    """
+    Handles subscription change and adding selected subscription
+    to the session
+    """
+    profile = UserProfile.objects.get(user=request.user)
+    if not profile.subscription:
+        return redirect(reverse('subscriptions'))
+
+    if not request.POST.get('subscription_type'):
+        return redirect(reverse('subscriptions'))
+
+    all_subscription = Subscription.objects.all()
+    subscription_type = request.POST.get('subscription_type')
+    request.session['subscription'] = subscription_type
+    subscription = get_object_or_404(Subscription, name=subscription_type)
+    template = 'subscription/subscription_checkout.html'
+
+    context = {
+        'change_subscription': True,
+        'subscription': subscription,
+        'all_subscription': all_subscription,
+    }
+    return render(request, template, context)
+
+
+@login_required
+def subscription_update(request):
+    """
+    Update user's subscription in the stripe system
+    and our database too
+    """
+
+    if not UserProfile.objects.get(user=request.user).subscription:
+        return redirect(reverse('subscription'))
+
+    stripe.api_key = settings.STRIPE_SECRET_KEY
+    # user's chosen subscription
+    subscription = request.session['subscription']
+
+    # Asign correct price keys to the paid subscriptions
+    if subscription == 'Gold':
+        price = settings.STRIPE_PRICE_ID_GOLD
+    elif subscription == 'Silver':
+        price = settings.STRIPE_PRICE_ID_SILVER
+    else:
+        price = settings.STRIPE_PRICE_ID_BRONZE
+
+    # Check if the user already exists in stripe system and
+    # our database
+    try:
+        stripe_customer = StripeCustomer.objects.get(user=request.user)
+        subscription = stripe.Subscription.retrieve(
+            stripe_customer.stripeSubscriptionId)
+        # Update existing subscription with a new one
+        stripe.Subscription.modify(
+            subscription.id,
+            cancel_at_period_end=False,
+            proration_behavior='create_prorations',
+            items=[{
+                'id': subscription['items']['data'][0].id,
+                'price': price,
+            }]
+        )
+
+        # Attach new subscription to the user's profile
+        subscription_type = get_object_or_404(Subscription, name=subscriptions)
+        profile = get_object_or_404(UserProfile, user=request.user)
+        profile.subscription = subscription_type
+        profile.save()
+
+        messages.success(request, 'Congrats!! You successfully changed'
+                                  ' your subscription to the '
+                                  f'{subscription} subscription!')
+        # Redirect the user to profiles page
+        return redirect(reverse('profile'))
+
+    # If user doesn't exist, return error
+    except StripeCustomer.DoesNotExist:
+        return messages.error(request, 'User does not exist')
